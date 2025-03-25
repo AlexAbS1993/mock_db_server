bfe1c15a69136dbb61cac8a4e981a64224b5c968
Alex Abetov <abetovalex93@gmail.com>
2025-03-26 02:03:10 +0300

    Исправлено создание строчки параметров в usersHandler

--- handlers/usersHandler.js
+++ handlers/usersHandler.js
@@ -2,13 +2,34 @@ class GetUsers {
     #limit
     #offset
     #USERS = 'users'
+    #query_string
     constructor(query){
         this.#limit = query.limit
         this.#offset = query.offset
     }
     async handle(db){
-        let users = await db.query(`SELECT * FROM ${this.#USERS}` + ';')
+        this.createQueryString()
+        let users = await db.query(this.#query_string + ';')
         return users
+    }
+    createQueryString(){
+        this.#query_string = `SELECT * FROM ${this.#USERS}`
+        this.buildQueryString()
+        return
+    }
+    buildQueryString(){
+        this.addLimit()
+        this.addOffset()
+    }
+    addLimit(){
+        if (this.#limit){
+            this.#query_string += ` LIMIT ${this.#limit}`
+        }
+    }
+    addOffset(){
+        if (this.#offset){
+            this.#query_string +=  ` OFFSET ${this.#offset}`
+        }
     }
 }
 
